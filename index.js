64df3c634269e73d2eebca1c6914128cea86205a
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-11-27 12:52:57 +0600

    Created gallery api applying pagination

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9lypro.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,30 +26,51 @@ async function run() {
     await client.connect();
 
     const classesCollection = client.db("fithubDB").collection("classes");
+    const photoCollection = client.db("fithubDB").collection("gallery");
+
+    //gallery api
+    //   app.get('/gallery', async(req, res) => {
+    //     const result = await photoCollection.find().toArray();
+    //     res.send(result);
+    // })
+
+    const PAGE_SIZE = 12; // Adjust this value based on the number of images you want to fetch per page
+
+    app.get("/gallery", async (req, res) => {
+      const { page = 1 } = req.query;
+      const skip = (page - 1) * PAGE_SIZE;
+
+      try {
+        const result = await photoCollection
+          .find()
+          .skip(skip)
+          .limit(PAGE_SIZE)
+          .toArray();
+        res.json({ images: result });
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        // res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
     //classes api
-    app.get('/classes', async(req, res) => {
-        const result = await classesCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/classes/:id', async(req, res) => {
+    app.get("/classes/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await classesCollection.findOne(query);
       res.send(result);
-    })
-
-
-
-
-
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -58,17 +78,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-app.get('/', async(req, res) => {
-    res.send("FitHub is running");
-})
+app.get("/", async (req, res) => {
+  res.send("FitHub is running");
+});
 
 app.listen(port, () => {
-    console.log(`FitHub is running on port: ${port}`);
-})
-
+  console.log(`FitHub is running on port: ${port}`);
+});
